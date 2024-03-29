5662bbc9bac02c24bfe012252c379f742d6af044
roozbeh <roozbehsamiee@gmail.com>
2024-03-30 01:29:46 +0330

    fix: fix environment variables

--- BlogApp/settings.py
+++ BlogApp/settings.py
@@ -90,9 +90,9 @@ WSGI_APPLICATION = 'BlogApp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DATABASE_NAME'),
-        'USER': os.environ.get('DATABASE_USER'),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
+        'NAME': os.environ.get('POSTGRES_DB'),
+        'USER': os.environ.get('POSTGRES_USER'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('DATABASE_HOST'),
         'PORT': os.environ.get('DATABASE_PORT'),
     }
