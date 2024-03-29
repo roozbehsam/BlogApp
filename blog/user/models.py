a1e553eee809e3fab931fb21f1d9788cf80e9aef
roozbeh <roozbehsamiee@gmail.com>
2024-03-29 22:37:41 +0330

    feat: add abstract manager to blog:user models

--- blog/user/models.py
+++ blog/user/models.py
@@ -1,19 +1,10 @@
-import uuid
-
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
-from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
-from django.http import Http404
+
+from blog.abstract.models import AbstractModel, AbstractManager
 
 
-class UserManager(BaseUserManager):
-    def get_object_by_public_id(self, public_id):
-
-        try:
-            instance = self.get(public_id=public_id)
-            return instance
-        except (ObjectDoesNotExist, ValueError, TypeError):
-            return Http404
+class UserManager(BaseUserManager, AbstractManager):
 
     def create_user(self, username, email, password=None, **kwargs):
         """Create and return a `User` with an email, phone number, username and password."""
@@ -49,8 +40,7 @@ class UserManager(BaseUserManager):
         return user
 
 
-class User(AbstractBaseUser, PermissionsMixin):
-    public_id = models.UUIDField(db_index=True, unique=True, default=uuid.uuid4, editable=False)
+class User(AbstractModel, AbstractBaseUser, PermissionsMixin):
     username = models.CharField(db_index=True, max_length=255, unique=True)
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
