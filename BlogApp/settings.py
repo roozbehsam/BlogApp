c0ab91675058cd698e57ed02ace80872acc4c4fb
roozbeh <roozbehsamiee@gmail.com>
2024-03-29 22:12:20 +0330

    feat: update settings.py
    added installed_apps, pagination for drf, jwt config

--- BlogApp/settings.py
+++ BlogApp/settings.py
@@ -35,6 +35,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # external packages apps
+
+    'rest_framework',
+    'rest_framework_simplejwt',
+
+    'core',
+    'core.user',
+    'core.auth',
+    'core.post'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +129,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'core_user.User'
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 10
+}
